6f52a93112e1fbc804960e5cc8299fa3f7680d29
Danone <mr.super.ko@inbox.ru>
2022-02-08 19:19:36 +0600

    First commit - map rescale

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PIL.ImageQt import QImage
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -12,11 +13,12 @@ class ConverterWindow(QMainWindow):
         super(ConverterWindow, self).__init__()
         self.setWindowTitle('Карта')
         uic.loadUi('main.ui', self)
+        self.scale = 1.0
         self.image_maps()
 
     def image_maps(self):
         map_request = f'https://static-maps.yandex.ru/1.x/?ll=73.368221,54.989347' \
-                      f'&spn=0.05,0.05&size=650,450&l=map&scale=1'
+                      f'&spn=0.05,0.05&size=650,450&l=map&scale={self.scale}'
         response = requests.get(map_request)
 
         if not response:
@@ -28,6 +30,17 @@ class ConverterWindow(QMainWindow):
             image = QImage.fromData(response.content)
             pixmap = QPixmap.fromImage(image)
             self.map.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.scale < 4.0:
+                self.scale += 0.1
+                self.image_maps()
+
+        elif event.key() == Qt.Key_PageDown:
+            if self.scale > 1.0:
+                self.scale -= 0.1
+                self.image_maps()
 
 
 if __name__ == '__main__':
