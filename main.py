e126772ad11b35ee6ea6803e0b244dc18a322610
Danone <mr.super.ko@inbox.ru>
2022-02-11 23:12:42 +0600

    add layers-button

--- main.py
+++ main.py
@@ -3,29 +3,36 @@ import requests
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PIL.ImageQt import QImage
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class ConverterWindow(QMainWindow):
+class MapWindow(QMainWindow):
     def __init__(self):
-        super(ConverterWindow, self).__init__()
+        super(MapWindow, self).__init__()
 
         self.setWindowTitle('Карта')
         uic.loadUi('main.ui', self)
         self.setFixedSize(650, 450)
 
+        self.layers_icon.setPixmap(QPixmap('media/layers_38px.png'))
+        self.menu_icon.setPixmap(QPixmap('media/menu_32px.png'))
+
+        self.layers_butt.clicked.connect(self.layers_change)
+        self.menu_butt.clicked.connect(self.menu_show)
+        self.search_field.clearFocus()
+
+        self.layer = 'map'
         self.spn = 0.01
         self.x = 73.368221
         self.y = 54.989347
-
-        self.map.setFocus(True)
+        self.menu_x = self.menuBox.x()
 
         self.image_maps()
 
     def image_maps(self):
         map_request = f'https://static-maps.yandex.ru/1.x/?ll={self.x},{self.y}' \
-                      f'&spn={self.spn},{self.spn}&size=650,450&l=map'
+                      f'&spn={self.spn},{self.spn}&size=650,450&l={self.layer}'
         response = requests.get(map_request)
         print(self.spn)
 
@@ -40,6 +47,23 @@ class ConverterWindow(QMainWindow):
             self.map.setPixmap(pixmap)
             response.close()
 
+    def layers_change(self):
+        if self.layer == 'map':
+            self.layer = 'sat'
+        elif self.layer == 'sat':
+            self.layer = 'sat,skl'
+        else:
+            self.layer = 'map'
+        self.image_maps()
+        self.map.setFocus(True)
+
+    def menu_show(self):
+        self.menu_x = self.menuBox.x()
+        if self.menu_x == -250:
+            self.menuBox.move(10, 50)
+        else:
+            self.menuBox.move(-250, 50)
+
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
             if self.spn > 0.0001:
@@ -47,40 +71,46 @@ class ConverterWindow(QMainWindow):
                 self.image_maps()
 
         elif event.key() == Qt.Key_PageDown:
-            if self.spn < 90:
+            if self.spn < 40:
                 self.spn = self.spn * 2
-                self.image_maps()
+            self.image_maps()
 
         elif event.key() == Qt.Key_Left:
+            self.map.setFocus(True)
             if self.x < 180.0 or self.x > -180.0:
-                self.x -= 0.01
+                self.x -= 0.01 * self.spn * 100
+                if self.x < -180:
+                    self.x = 180 - abs(self.x + 180)
+                    print(self.x)
                 self.image_maps()
 
         elif event.key() == Qt.Key_Right:
+            self.map.setFocus(True)
             if self.x < 180.0 or self.x > -180.0:
-                self.x += 0.01
+                self.x += 0.01 * self.spn * 100
+                if self.x > 180:
+                    self.x = -180 + abs(self.x - 180)
                 self.image_maps()
 
         elif event.key() == Qt.Key_Up:
-            if self.y < 90.0 or self.x > -90.0:
-                self.y += 0.01
-                self.image_maps()
+            self.map.setFocus(True)
+            if self.y < 90.0:
+                self.y += 0.01 * self.spn * 100
+                if self.y > 90:
+                    self.y = 90
+            self.image_maps()
 
         elif event.key() == Qt.Key_Down:
-            if self.y < 90.0 or self.x > -90.0:
-                self.y -= 0.01
-                self.image_maps()
-
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.menuBox.move(10, 50)
-
-        elif event.button() == Qt.RightButton:
-            self.menuBox.move(-250, 50)
+            self.map.setFocus(True)
+            if self.x > -90.0:
+                self.y -= 0.01 * self.spn * 100
+                if self.y < -90:
+                    self.y = -90
+            self.image_maps()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    wnd = ConverterWindow()
+    wnd = MapWindow()
     wnd.show()
     sys.exit(app.exec())
